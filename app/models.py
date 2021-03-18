b0bdb77a6e0a4ed0dad57cae00a6b65979a53dfa
dtekluva <kboysreel@gmail.com>
2021-03-18 01:56:55 +0100

    minor tweaks

--- app/models.py
+++ app/models.py
@@ -8,3 +8,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+       
+class SecondUser(models.Model):
+    phone_number = models.CharField(max_length=20, default="0")
+    is_ceo = models.BooleanField(default=False)
+    is_ppl_staff = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
+
+   
